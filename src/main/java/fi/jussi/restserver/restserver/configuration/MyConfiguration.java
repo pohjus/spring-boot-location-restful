5cff9b137145fb842c2e431fe886cc37b4dab045
pohjus <jussi.pohjolainen@tuni.fi>
2020-01-15 17:13:37 +0200

    Configuration depricated fix

--- src/main/java/fi/jussi/restserver/restserver/configuration/MyConfiguration.java
+++ src/main/java/fi/jussi/restserver/restserver/configuration/MyConfiguration.java
@@ -2,16 +2,25 @@ package fi.jussi.restserver.restserver.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-/**
- * Class Description.
- *
- * @author Jussi Pohjolainen
- * @version 2017-10-09
- */
+@Configuration
+@EnableWebMvc
+public class MyConfiguration implements WebMvcConfigurer {
+ 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "OPTIONS", "POST", "PUT", "DELETE");
+    }
+}
+
+/*
 @Configuration
 public class MyConfiguration {
 
@@ -26,4 +35,4 @@ public class MyConfiguration {
             }
         };
     }
-}
+}*/
