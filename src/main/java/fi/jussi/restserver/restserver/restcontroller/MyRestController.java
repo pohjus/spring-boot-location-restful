bddfb1de6ab19f7bc7e128e0c432d71370600ef4
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-07-16 13:49:38 +0300

    Add javadoc.

--- src/main/java/fi/jussi/restserver/restserver/restcontroller/MyRestController.java
+++ src/main/java/fi/jussi/restserver/restserver/restcontroller/MyRestController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import javax.annotation.PostConstruct;
 import java.util.Optional;
 
+/**
+ * REST Controller.
+ */
 @CrossOrigin
 @RestController
 public class MyRestController {
@@ -33,6 +36,14 @@ public class MyRestController {
     }
 
     // curl http://localhost:8080/api/locations/1
+
+    /**
+     * HTTP GET for one location.
+     *
+     * @param locationId given id from the url.
+     * @return status code 200 and the resource.
+     * @throws CannotFindLocationException if location is not found.
+     */
     @RequestMapping(value = "/api/locations/{locationId}",  method = RequestMethod.GET)
     public ResponseEntity<Location> fetchLocation(@PathVariable long locationId) throws CannotFindLocationException {
         Optional<Location> location = database.findById(locationId);
@@ -44,23 +55,40 @@ public class MyRestController {
 
     }
 
-    // curl http://localhost:8080/api/locations/
+    /**
+     * HTTP GET for all locations.
+     *
+     * @return all the locations.
+     */
     @RequestMapping(value = "/api/locations/",  method = RequestMethod.GET)
     public Iterable<Location> fetchAll() {
         return database.findAll();
     }
 
-    // curl -X DELETE http://localhost:8080/api/locations/1
+    /**
+     * HTTP Delete one location.
+     *
+     * @param locationId location to be deleted.
+     * @return status code no content (204).
+     * @throws CannotFindLocationException if location is not found.
+     */
     @RequestMapping(value = "/api/locations/{locationId}",  method = RequestMethod.DELETE)
     public ResponseEntity<Void> deleteLocation(@PathVariable long locationId) throws CannotFindLocationException {
         if(!database.existsById(locationId))
             throw new CannotFindLocationException(locationId);
 
         database.deleteById(locationId);
-        return new ResponseEntity<Void>(HttpStatus.OK);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 
-    // curl -H "Content-type: application/json" -X POST -d "{\"latitude\": 90, \"longitude\": 90}",  http://localhost:8080/api/locations/
+
+    /**
+     * HTTP POST (add) new location.
+     *
+     * @param location HTTP Request object in json that is added to the database.
+     * @param uriComponentsBuilder To create http status location with proper url.
+     * @return The added object and status code 201.
+     */
     @RequestMapping(value = "/api/locations/",  method = RequestMethod.POST)
     public  ResponseEntity<Location> save(@RequestBody Location location, UriComponentsBuilder uriComponentsBuilder) {
         database.save(location);
