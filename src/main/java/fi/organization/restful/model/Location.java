f623e3a3cdbb153cc1abeb4fac94ebb1bf6a88c3
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-07-16 14:42:46 +0300

    Update lombok with arguments.

--- src/main/java/fi/organization/restful/model/Location.java
+++ src/main/java/fi/organization/restful/model/Location.java
@@ -1,9 +1,6 @@
 package fi.organization.restful.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -19,18 +16,15 @@ import javax.persistence.Id;
 @Getter
 @Setter
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class Location {
 
     @Id
     @GeneratedValue
     private long id;
 
+    @NonNull
     private double latitude;
+    @NonNull
     private double longitude;
-
-
-    public Location(double latitude, double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
 }
