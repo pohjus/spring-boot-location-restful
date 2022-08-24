8f67d0c0300343341f8c128ce052f60ae4c31e07
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-08-24 11:15:57 +0300

    Update Java Bean information

--- src/main/java/fi/organization/restful/model/Location.java
+++ src/main/java/fi/organization/restful/model/Location.java
@@ -1,10 +1,12 @@
 package fi.organization.restful.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 /**
  * Pojo for the RESTful api.
@@ -22,12 +24,16 @@ public class Location {
     public static final short LONGITUDE_MIN = -180;
     public static final short LONGITUDE_MAX = +180;
 
+    @Schema(description="Auto generated id", required = true)
     @Id
     @GeneratedValue
     private long id;
 
     @NonNull
+    @Size(min = -90, max = 90)
     private double latitude;
+
     @NonNull
+    @Size(min = -180, max = 180)
     private double longitude;
 }
